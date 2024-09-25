0e4810f67e74eb27ec90f7a4acb2a4686672174d
Vikas <7014.vikas@gmail.com>
2024-09-25 11:16:24 +0530

    react-components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DeleteButton from "./components/buttons/DeleteButton/DeleteButton";
+import TrashButton from "./Components/Buttons/TrashButton/TrashButton";
+
+export const App = () => {
+  return (
+    <div>
+      <DeleteButton />
+      <TrashButton />
+    </div>
+  );
+};
+
+export default App;
