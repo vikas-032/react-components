a013ce61eb59a7e5f65e410efdb2187ed2e917db
Vikas <7014.vikas@gmail.com>
2024-09-25 11:25:04 +0530

    component created successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
 import React from "react";
 import DeleteButton from "./components/buttons/DeleteButton/DeleteButton";
 import TrashButton from "./Components/Buttons/TrashButton/TrashButton";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import "./App.css";
 
-export const App = () => {
+const routes = [
+  {
+    name: "DeleteButton",
+    path: "/buttons/delete-button",
+    element: <DeleteButton />,
+  },
+  {
+    name: "TrashButton",
+    path: "/buttons/trash-button",
+    element: <TrashButton />,
+  },
+];
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+  },
+  ...routes,
+]);
+
+function Layout() {
   return (
-    <div>
-      <DeleteButton />
-      <TrashButton />
-    </div>
+    <main className="app">
+      {routes.map((route) => (
+        <Link key={route.path} to={route.path}>
+          {route.name}
+        </Link>
+      ))}
+    </main>
   );
-};
+}
+
+function App() {
+  return <RouterProvider router={router} />;
+}
 
 export default App;
